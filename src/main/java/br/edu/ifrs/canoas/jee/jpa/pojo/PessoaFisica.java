b7286ff865fcf2ea8d998166f570e1a7ffd57f8a
William José <williamjmrosa@gmail.com>
2019-10-13 19:37:58 -0300

    Classe PessoaFisica

---
+++ src/main/java/br/edu/ifrs/canoas/jee/jpa/pojo/PessoaFisica.java
@@ -0,0 +1,31 @@
+package br.edu.ifrs.canoas.jee.jpa.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Entity implementation class for Entity: PessoaFisica
+ *
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PessoaFisica extends Pessoa implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	private String cpf;
+	private String rg;
+	private Date dataNascimento;
+	
+}
