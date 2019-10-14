4fb5abe48abb08ba7194670b9243c36bf06451f9
William José <williamjmrosa@gmail.com>
2019-10-14 19:43:45 -0300

    Classe Diaria Reservada

--- src/main/java/br/edu/ifrs/canoas/jee/jpa/pojo/DiariaReservada.java
+++ src/main/java/br/edu/ifrs/canoas/jee/jpa/pojo/DiariaReservada.java
@@ -3,19 +3,27 @@ package br.edu.ifrs.canoas.jee.jpa.pojo;
 import java.io.Serializable;
 import javax.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Entity implementation class for Entity: DiariaReservada
  *
  */
 @Entity
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class DiariaReservada extends Diaria implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
 
-	public DiariaReservada() {
-		super();
-	}
+	@OneToOne (mappedBy = "diarias")
+	private Reserva reserva;
+	
    
 }
