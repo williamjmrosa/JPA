def3e6595cccf279495ace76d9643bf33a156849
William José <williamjmrosa@gmail.com>
2019-10-14 19:43:33 -0300

    Classe Diaria Avulsa

--- src/main/java/br/edu/ifrs/canoas/jee/jpa/pojo/DiariaAvulsa.java
+++ src/main/java/br/edu/ifrs/canoas/jee/jpa/pojo/DiariaAvulsa.java
@@ -3,19 +3,30 @@ package br.edu.ifrs.canoas.jee.jpa.pojo;
 import java.io.Serializable;
 import javax.persistence.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Helper;
+
 /**
  * Entity implementation class for Entity: DiariaAvulsa
  *
  */
 @Entity
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class DiariaAvulsa extends Diaria implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
 
-	public DiariaAvulsa() {
-		super();
-	}
+	@ManyToOne
+	@JoinColumn (name = "id_Pessoa")
+	private Pessoa cliente;
+	
+	
    
 }
